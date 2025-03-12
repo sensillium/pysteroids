ced67ce6954468e564c8d4092855b215d29a8181
Richard Mitchell <richard@apidaya.com>
2025-03-12 09:43:41 +0000

    player finished

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
-from pygame.locals import *
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill('black')
+        updatable.update(dt)
 
+        screen.fill('black')
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60Hz
+        delta = clock.tick(60)
+        dt = delta / 1000
 
 if __name__ == "__main__":
     main()
-
